86171086742632e145922f028994a72248084c0a
Rachel Powe <rachelpowe@gmail.com>
2020-07-28 15:08:18 -0400

    updates

--- src/Components/wineinfo.js
+++ src/Components/wineinfo.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+const BASE_URL = "http://myapi-profstream.herokuapp.com/api/a5b159/wines"
 
-//Render all Wines in the webpage
-export default class App extends React.Component {
+
+//Single Wine Routed Page
+export default class WineInfo extends React.Component {
     constructor() {
         super();
-        this.state = {
+        this.state ={
             wines: [],
         }
     }
 
     componentDidMount() {
-        axios.get("http://myapi-profstream.herokuapp.com/api/a5b159/wines")
+        axios.get(BASE_URL + {this.state.id})
         .then(res => this.setState({ wines: res.data }))
         .catch(error => console.error( error.message ));
     }
 
     render() {
         return (
-          <Router>
             <div>
                 <h1 className = "header" >WINE</h1>
                 <div className="container">
@@ -35,19 +29,15 @@ export default class App extends React.Component {
                         console.log(wine);
                         return (
                             <li className="wine">
-                            <Link to = { `/${wine.id}`}>
                                 <img src={wine.picture} alt="wine" />
                                 <h2>{wine.name}</h2>
                                 {/* <h3>{wine.description}</h3> */}
-                                </Link>
                             </li>
                         )
-                    }
-                  )
+                    })
                 }
                 </div>
             </div>
-          </Router>
         )
     }
 }
